78f5bb769a9f63905b27750a00d44b4951874235
Paul Bredenberg <paulbredenberg@gmail.com>
2021-08-13 17:59:52 -0400

    wip: string replacements

--- src/options/upgrade.ts
+++ src/options/upgrade.ts
@@ -1,7 +1,30 @@
-const upgrade = (): void => {
-   const cwd = process.cwd();
+import { replaceInFile, ReplaceInFileConfig } from 'replace-in-file';
 
-   console.log('cwd', cwd);
+interface FileStringReplacementSet {
+   fileName: string;
+   stringPattern: RegExp;
+}
+
+const upgrade = async (): Promise<void> => {
+   const cwd = process.cwd(),
+         nodeVersion = '12.14.0',
+         npmVersion = '6.13.4';
+
+   let files: string[];
+
+   files = [ 'package.json', '.nvmrc' ];
+
+   await Promise.all(files.map(async (file) => {
+      let options: ReplaceInFileConfig;
+
+      options = {
+         files: `${cwd}/${file}`,
+         from: new RegExp(`check-node-version --npm ${npmVersion}`, 'g'),
+         to: 'check-node-version --npm foo',
+      };
+
+      await replaceInFile(options);
+   }));
 };
 
 export default upgrade;
