f1c4244472363f9ed5689efbbbe02fb2bce71c12
Paul Bredenberg <paulbredenberg@gmail.com>
2021-08-14 09:10:55 -0400

    feat: add collection of files and replacement patterns

--- src/options/upgrade.ts
+++ src/options/upgrade.ts
@@ -1,26 +1,39 @@
 import { replaceInFile, ReplaceInFileConfig } from 'replace-in-file';
 
-interface FileStringReplacementSet {
-   fileName: string;
-   stringPattern: RegExp;
-}
-
 const upgrade = async (): Promise<void> => {
    const cwd = process.cwd(),
          nodeVersion = '12.14.0',
          npmVersion = '6.13.4';
 
-   let files: string[];
+   let configs: ReplaceInFileConfig[];
 
-   files = [ 'package.json', '.nvmrc' ];
+   configs = [
+      {
+         files: [ 'package.json' ],
+         from: new RegExp(`--npm ${npmVersion}`, 'g'),
+         to: '--npm 6.14.12',
+      },
+      {
+         files: [ 'package.json' ],
+         from: new RegExp(`--node ${nodeVersion}`, 'g'),
+         to: '--node 12.22.1',
+      },
+      {
+         files: [ '.nvmrc' ],
+         from: new RegExp(`${nodeVersion}`, 'g'),
+         to: '12.22.1',
+      },
+   ];
 
-   await Promise.all(files.map(async (file) => {
+   await Promise.all(configs.map(async (config) => {
       let options: ReplaceInFileConfig;
 
       options = {
-         files: `${cwd}/${file}`,
-         from: new RegExp(`check-node-version --npm ${npmVersion}`, 'g'),
-         to: 'check-node-version --npm foo',
+         files: (config.files as string[]).map((file: string) => {
+            return `${cwd}/${file}`;
+         }),
+         from: config.from,
+         to: config.to,
       };
 
       await replaceInFile(options);
