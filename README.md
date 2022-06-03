ec2915db1cbd2008ce1d061f0d0d7b33633d8e36
Paul Bredenberg <paulbredenberg@gmail.com>
2022-06-03 10:47:48 -0400

    feat: remove package lock before npm ci in upgrade

--- README.md
+++ README.md
@@ -10,7 +10,13 @@
 
 ## What?
 
+File templates and utilities to help maintain projects for Silvermine.
+
 ## Why?
+
+Since most of our projects contain configuration that is virtually identical, we can standardize upgrades
+of these projects by providing utility scripts and file templates that make it less manual and cumbersome
+to upgrade common software versions.
 
 ## License
 
--- src/options/upgrade.ts
+++ src/options/upgrade.ts
@@ -1,8 +1,12 @@
 import { replaceInFile, ReplaceInFileConfig } from 'replace-in-file';
 import { REPLACEMENT_TARGET_FILE_CONFIGS } from '../constants';
 import { executeCommand } from '../utilities/execute-command';
+import fs from 'fs';
+import { promisify } from 'util';
+import path from 'path';
 
 const upgrade = async (): Promise<void> => {
+   const rm = promisify(fs.unlink);
    const cwd = process.cwd();
 
    await Promise.all(REPLACEMENT_TARGET_FILE_CONFIGS.map(async (config) => {
@@ -32,13 +36,22 @@ const upgrade = async (): Promise<void> => {
       }
    }));
 
-   console.log('Running `npm ci`...');
+   console.log('Removing package-lock.json in preparation for upgrade...');
+
+   try{
+      await rm(path.resolve(process.cwd(), 'package-lock.json'));
+      console.log('package-lock.json removed')
+   } catch (e) {
+      console.error('Could not remove package-lock.json:', e);
+   }
+
+   console.log('Running `npm i`...');
 
    try {
-      await executeCommand('npm ci');
+      await executeCommand('npm i');
    } catch(e) {
       console.error(e);
-      throw new Error('Error running `npm ci`');
+      throw new Error('Error running `npm i`');
    }
 };
 
